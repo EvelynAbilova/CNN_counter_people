c462e6fbe5a95aa3899d0d4d08320fffadf83430
EvelynAbilova <79151653+EvelynAbilova@users.noreply.github.com>
2023-04-08 14:58:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,29 +15,24 @@ model = tf.keras.Sequential([
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(16, activation='relu'),
     tf.keras.layers.Dense(1, activation='relu')
 ])
 
 model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
 # Train the model
-batch_size = 32
+batch_size = 8
 train_steps = len(labels_df) // batch_size
-EPOCHS = 10
+EPOCHS = 25
 
-train_generator = data_gen.data_generator(batch_size=batch_size)
+train_generator = data_gen.data_generator(batch_size)
 
 model.fit(train_generator, steps_per_epoch=train_steps, epochs=EPOCHS, verbose=1)
 
-# Evaluate the model on a test set
-test_dataset = data_gen.data_generator(batch_size=32)
-test_steps = len(labels_df) // 32
-test_loss, test_mae = model.evaluate(test_dataset, steps=test_steps)
-
-print(f"Test loss: {test_loss:.4f}")
-print(f"Test MAE: {test_mae:.4f}")
-
 model.save('cnn_people_counter.h5')
-
+print('Model saved')
