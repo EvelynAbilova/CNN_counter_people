6ab4862978ab281751259af76e53dda28f2e1f40
EvelynAbilova <79151653+EvelynAbilova@users.noreply.github.com>
2023-04-20 21:52:33 +0300

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -5,10 +5,9 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-
-# Load the dataset
 import data_gen
 
+MALL_DIR = "MALL/"
 frames_dir = "MALL/frames/frames"
 labels_file = "MALL/labels.csv"
 
@@ -49,6 +48,7 @@ test_loss, test_mae = model.evaluate(test_dataset, steps=test_steps)
 # Compute the predictions on the test set
 y_true = np.concatenate([next(test_dataset)[1] for i in range(test_steps)])
 y_pred = model.predict(test_dataset, steps=test_steps).flatten()
+sorted_indices = np.argsort(y_pred)
 
 # Compute the metrics
 mae = mean_absolute_error(y_true, y_pred)
@@ -60,3 +60,66 @@ print(f"Test MAE: {test_mae:.4f}")
 print(f"MAE: {mae:.4f}")
 print(f"MSE: {mse:.4f}")
 print(f"Relative Error: {relative_error:.2f}%")
+
+# Load the test dataset
+test_dataset = data_gen.data_generator(batch_size=8)
+
+# Predict on the test dataset
+y_true = []
+y_pred = []
+
+for i, (X, y) in enumerate(test_dataset):
+    y_true += y.tolist()
+    y_pred += model.predict(X).flatten().tolist()
+
+    if (i + 1) * 8 >= len(labels_df):
+        break
+
+# Convert lists to numpy arrays
+y_true = np.array(y_true)
+y_pred = np.array(y_pred)
+
+# Compute the absolute errors
+errors = np.abs(y_pred - y_true)
+
+# Save the predicted and actual counts for each test sample
+results = []
+for i in range(len(y_true)):
+    img_path = os.path.join(MALL_DIR, 'frames', 'frames', 'seq_{:06d}.jpg'.format(i+1))
+    actual_count = int(y_true[i])
+    predicted_count = int(round(y_pred[i]))
+    results.append((img_path, predicted_count, actual_count))
+
+# Sort the results by the difference between actual count and predicted count
+results.sort(key=lambda x: abs(x[2] - x[1]))
+
+# Select the best and worst results based on prediction accuracy
+best_result = results[0]
+worst_result = results[-1]
+
+# Calculate the prediction accuracy for the best and worst results
+best_accuracy = (1 - abs(best_result[1] - best_result[2]) / best_result[1]) * 100
+worst_accuracy = (1 - abs(worst_result[2] - worst_result[1]) / worst_result[2]) * 100
+
+# Display the best and worst results
+plt.figure(figsize=(10, 5))
+
+# Display the best result
+plt.subplot(1, 2, 1)
+img = cv2.imread(best_result[0], cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.axis('off')
+plt.title('Best score\nActual count: {}\nPredicted count: {}\nAccuracy: {:.2f}%'.format(
+    best_result[2], best_result[1], best_accuracy))
+
+# Display the worst result
+plt.subplot(1, 2, 2)
+img = cv2.imread(worst_result[0], cv2.IMREAD_COLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+plt.imshow(img)
+plt.axis('off')
+plt.title('Worst score\nActual count: {}\nPredicted count: {}\nAccuracy: {:.2f}%'.format(
+    worst_result[2], worst_result[1], worst_accuracy))
+
+plt.show()
